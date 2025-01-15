d2bdf54858b209d65dc8ea6e53b7cd77ac1fd8ce
tegen-ux <117418989+tegen-ux@users.noreply.github.com>
2025-01-15 16:33:24 -0700

    Actually might be done this time :)

--- src/app/src/main/java/com/example/listycity/MainActivity.java
+++ src/app/src/main/java/com/example/listycity/MainActivity.java
@@ -6,26 +6,37 @@ import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     //Declare the variables that we'll use later.
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        // First we find the reference to ListView, then we set it to cityList
+        cityList = findViewById(R.id.city_list);
+        // Then we can make a String Array consiting of awesome cities
+        String[] cities = {"Edmonton", "Red Deer", "Calgary", "Fort MacMurray", "Sylvan Lake",
+                "Vancouver", "Spruce Grove", "Camrose", "Wetaskiwin", "Poggers", "New York"};
+        // Then we can declare a new arrayList
+        dataList = new ArrayList<>();
+
+        // Then we all all of our cities into that array
+        dataList.addAll(Arrays.asList(cities));
+        // Then we do some adapter thing, which I have no clue what it does
+        // All I know is that it links all this code to the content.xml that we had earlier.
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        // Same again, I have no clue what is there
+        cityList.setAdapter(cityAdapter);
+
+
     }
 }
